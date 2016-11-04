36964de626f3214b7f223033f8c700bfc01e668e
Hasnat Ullah <hullah@afilias.info>
2016-11-04 15:21:14 +0000

    Added csv db and script to convert it to loki db

---
+++ dev_scripts/buildDb.js
@@ -0,0 +1,12 @@
+import loki from 'lokijs';
+import fs from 'fs';
+var csvData = fs.readFileSync('./db/traffic_data.csv').toString();
+var db = new loki('./db/traffic_data.json')
+var trafficCollection = db.addCollection('traffic_collection')
+
+csvData.split("\n").map((line) => {
+	var [country_code,browser_name,browser_version,os_name,os_version,traffic] = line.split(',');
+	browser_name = browser_name.replace(/"/g, '');
+	trafficCollection.insert({country_code,browser_name,browser_version,os_name,os_version,traffic});
+});
+db.saveDatabase();
